a3f8a08184901f3293501d6829746d6303433571
Anton Skorokhod <anton@nsl.cz>
2016-06-21 00:25:04 +0200

    Example update

--- examples/requestParse/generator.go
+++ examples/requestParse/generator.go
@@ -10,6 +10,7 @@ type reqProfile struct {
 	ID       int64  `req:"id,nempty,nzero",json:"id"`
 	Name     string `req:"name,sphinx,nempty,len>3,len<64",json:"name"`
 	Language string `req:"language,nempty,len=2",json:"language"`
+	Params   map[string]string `req:"prefix=param_",json:"params"`
 	Groups   map[string][]string `req:"prefix=group_",json:"groups"`
 	Passwd   string `req:"password,len>8",json:"-"`
 }
@@ -55,7 +56,52 @@ func main() {
 				checks = f.Tags[1:]
 			}
 
-			if f.Type != "map[string][]string" {
+			if f.Type == "map[string][]string" {
+				prefix, ok := f.TagParams["prefix"]
+				if !ok {
+					log.Fatalln("no prefix for map[string][]string field", f.Name, "in struct", st.Name)
+				}
+				prefixLen := len(prefix)
+				fmt.Printf("\tres.%s = map[string][]string{}\n\n", f.Name)
+				fmt.Printf("\tfor k, v := range request.Form {\n\t\tif strings.HasPrefix(k, \"%s\") {\n\t\t\tres.%s[k[%d:]] = v\n\t\t}\n\t}\n", prefix, f.Name, prefixLen)
+			} else if f.Type == "map[string]string" {
+				prefix, ok := f.TagParams["prefix"]
+				if !ok {
+					log.Fatalln("no prefix for map[string]string field", f.Name, "in struct", st.Name)
+				}
+				prefixLen := len(prefix)
+				fmt.Printf("\tres.%s = map[string]string{}\n\n", f.Name)
+				fmt.Printf("\tfor k, v := range request.Form {\n\t\tif strings.HasPrefix(k, \"%s\") {\n\t\t\tres.%s[k[%d:]] = v[0]\n\t\t}\n\t}\n", prefix, f.Name, prefixLen)
+
+			} else if f.Type == "[]int64" {
+				fmt.Printf("\tres.%s = []int64{}\n\n", f.Name)
+				_, ok := f.TagParams["jarray"]
+				if ok {
+					fmt.Printf("\tparam%s := strings.Split(request.Form.Get(\"%s\"), \",\")\n", f.Name, pname)
+					fmt.Printf("\tfor _, _x := range param%s {\n", f.Name)
+					fmt.Print("\t\tx, err := strconv.ParseInt(_x, 10, 64)\n")
+					fmt.Printf("\t\tif nil == err {\n")
+					fmt.Printf("\t\t\tres.%s = append(res.%s, x)\n", f.Name, f.Name)
+					fmt.Print("\t\t}\n")
+					fmt.Print("\t}\n")
+
+					// pre convert chcecks
+					for _, c := range checks {
+						switch c {
+						case "nempty":
+							fmt.Printf("\tif len(res.%s) == 0 {\n\t\treturn %s{}, \"%s_empty\"\n\t}\n", f.Name, st.Name, pname)
+						}
+					}
+
+				} else {
+
+				}
+
+				if !ok {
+					log.Fatalln("no prefix for map[string]string field", f.Name, "in struct", st.Name)
+				}
+
+			} else {
 
 				if !strings.HasPrefix(f.Type, "[]") {
 					fmt.Printf("\tparam%s := request.Form.Get(\"%s\")\n", f.Name, pname)
@@ -94,28 +140,28 @@ func main() {
 				case "[]string":
 					fmt.Printf("\tres.%s = param%s\n\n", f.Name, f.Name)
 				case "int":
-					fmt.Printf("\tres.%s = strconv.Atoi(param%s)\n\n", f.Name, f.Name)
+					fmt.Printf("\tres.%s, err = strconv.Atoi(param%s)\n\n", f.Name, f.Name)
 				case "int64":
 					if !errDefined {
-						fmt.Println("\tvar err error\n")
+						fmt.Print("\tvar err error\n\n")
 						errDefined = true
 					}
 					fmt.Printf("\tres.%s, err = strconv.ParseInt(param%s, 10, 64)\n\n", f.Name, f.Name)
 				case "uint64":
 					if !errDefined {
-						fmt.Println("\tvar err error\n")
+						fmt.Print("\tvar err error\n\n")
 						errDefined = true
 					}
 					fmt.Printf("\tres.%s, err = strconv.ParseUint(param%s, 10, 64)\n\n", f.Name, f.Name)
 				case "float64":
 					if !errDefined {
-						fmt.Println("\tvar err error\n")
+						fmt.Print("\tvar err error\n\n")
 						errDefined = true
 					}
 					fmt.Printf("\tres.%s, err = strconv.ParseFloat(param%s, 64)\n\n", f.Name, f.Name)
 				case "bool":
 					if !errDefined {
-						fmt.Println("\tvar err error\n")
+						fmt.Print("\tvar err error\n\n")
 						errDefined = true
 					}
 					fmt.Printf("\tres.%s, err = strconv.ParseBool(param%s)\n\n", f.Name, f.Name)
@@ -130,14 +176,19 @@ func main() {
 						fmt.Printf("\tif 0 == res.%s {\n\t\treturn %s{}, \"%s_invalid\"\n\t}\n", f.Name, st.Name, pname)
 					}
 				}
-			} else {
-				prefix, ok := f.TagParams["prefix"]
-				if !ok {
-					log.Fatalln("no prefix for map[string][]string field", f.Name, "in struct", st.Name)
+
+				if tagVal, ok := f.TagParams["val"]; ok {
+					fmt.Printf("\tif %s != res.%s {\n\t\treturn %s{}, \"%s_invalid\"\n\t}\n", tagVal, f.Name, st.Name, pname)
 				}
-				prefixLen := len(prefix)
-				fmt.Printf("\tres.%s = map[string][]string{}\n\n", f.Name)
-				fmt.Printf("\tfor k, v := range request.Form {\n\t\tif strings.HasPrefix(k, \"%s\") {\n\t\t\tres.%s[k[%d:]] = v\n\t\t}\n\t}\n", prefix, f.Name, prefixLen)
+
+				if tagVal, ok := f.TagGt["val"]; ok {
+					fmt.Printf("\tif %d > res.%s {\n\t\treturn %s{}, \"%s_invalid\"\n\t}\n", tagVal, f.Name, st.Name, pname)
+				}
+
+				if tagVal, ok := f.TagLt["val"]; ok {
+					fmt.Printf("\tif %d < res.%s {\n\t\treturn %s{}, \"%s_invalid\"\n\t}\n", tagVal, f.Name, st.Name, pname)
+				}
+
 			}
 
 		}
